a83f4be4e6b8a43de523782668a96b3cb4b274c8
blartblart <60076991+blartblart@users.noreply.github.com>
2023-04-30 21:44:14 -0700

    desert js changed

--- desertscripts.js
+++ desertscripts.js
@@ -24,19 +24,21 @@ function loadFileInto(fromFile, whereTo) {
 }
 
 //define a recipe object constructor
-function Recipe(a, b, c, d) {
+function Recipe(a, b, c, d, e) {
 
     //set object properties
     this.name = a;
-    this.ingredientsFile = b; // file naem to the HTML snippet containg this recipe's ingredients list
-    this.directionsFile = c; //file neame to the HTML snippet containg this recipe's directions list 
-    this.imageSource = d; // URL or file name to teh recipe photo
+    this.ingredientsFile = b; // file name to the HTML snippet containing this recipe's ingredients list
+    this.directionsFile = c; //file name to the HTML snippet containing this recipe's directions list 
+    this.descriptionFile = d; //file name to the HTML snippent containing this recipe's description
+    this.imageSource = e; // URL or file name to teh recipe photo
 
     //update the display with the content for this recipe
     this.display = function() {
         document.querySelector("#hero h1").innerHTML = this.name;
         loadFileInto(this.ingredientsFile, "#ing");
         loadFileInto(this.directionsFile, "#dir");
+        loadFileInto(this.descriptionFile, "#des");
         document.querySelector("#hero").style.backgroundImage = "url(" + this.imageSource + ")";
         document.title = "Recipe: " + this.name;
     }// end of .display() method
@@ -57,29 +59,43 @@ function Recipe(a, b, c, d) {
     }// end of .addToNav() method
 }
 
-let shakshuka = new Recipe( "Drake's Shakshuka",
-                            "ingredients.html", 
-                            "directions.html", 
-                            "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F9055595.jpg&q=60&c=sc&orient=true&poi=auto&h=512");
+let dDesert = new Recipe( "Drake's Coffee Jelly",
+                            "d/dDesertIng.html", 
+                            "d/dDesertDir.html", 
+                            "d/dDesertDes.html",
+                            "https://elnazarov.reclaim.hosting/shelter/ddesert.png");
 
-let tunaSalad = new Recipe( "Barbie's Tuna Salad",
-                            "tunaing.html", 
-                            "tunadir.html", 
-                            "https://www.cookingclassy.com/wp-content/uploads/2020/03/tuna-salad-3.jpg");
+let eDesert = new Recipe( "Ernie's Raspberry Key-Lime Cheesecake Bites",
+                            "e/eDesertIng.html", 
+                            "e/eDesertDir.html", 
+                            "e/eDesertDes.html",
+                            "https://elnazarov.reclaim.hosting/shelter/edesert.png");
 
-let cobbler = new Recipe(  "Patricia's Blackberry Cobbler",
-                            "cobblering.html", 
-                            "cobblerdir.html", 
+let aDesert = new Recipe(  "Aspen's Lemon Poppy Scones",
+                            "a/aDesertIng.html", 
+                            "a/aDesertDir.html", 
+                            "a/aDesertDes.html",
+                            "https://elnazarov.reclaim.hosting/shelter/adesert.png");
+
+let bDesert = new Recipe(  "Barbie's No-Bake Cookies",
+                            "b/bDesertIng.html", 
+                            "b/bDesertDir.html", 
+                            "b/bDesertDes.html",
+                            "https://elnazarov.reclaim.hosting/shelter/bdesert.png");
+
+let cDesert = new Recipe(  "Colleen's Berry Cobbler",
+                            "c/cDesertIng.html", 
+                            "c/cDesertDir.html", 
+                            "c/cDesertDes.html",
                             "https://www.cookingmamas.com/wp-content/uploads/2012/03/Blackberry-Cobbler-1024x793.jpg");
-
-
 
 // don't do anything until the DOM loads
 document.addEventListener("DOMContentLoaded", function() {
     
-    shakshuka.addToNav();
-    tunaSalad.addToNav();
-    cobbler.addToNav();
+    dDesert.addToNav();
+    eDesert.addToNav();
+    aDesert.addToNav();
+    bDesert.addToNav();
+    cDesert.addToNav();
 
-    
 });
